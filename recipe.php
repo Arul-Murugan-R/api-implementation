b292f1d84ea564f096e50b14a6fc5c8b5e8f6b0a
Arul-Murugan-R <arulmuruganarjun14@gmail.com>
2021-09-29 12:55:25 +0530

    pop up content added

--- recipe.php
+++ recipe.php
@@ -23,8 +23,9 @@
             cursor:pointer;
         }
         .display-recipe{
-            padding:0px 100px;
+            /* padding:0px 100px; */
             display:flex;
+            justify-content:space-around;
             flex-wrap:wrap;
         }
         .recipe-card{
@@ -47,8 +48,7 @@
         .try-new{
             border-radius:25px;
             padding:10px;
-            background-color:lightblue;
-            font-weight:bold;
+            background-color:orange;
             border:0px;
             width:100px;
             cursor:pointer;
@@ -56,9 +56,105 @@
         .recipe-info{
             padding:10px;
         }
+        /* @media (max-width:600px){
+            .display-recipe{
+                padding:30px;
+                padding-left:70px;
+            }
+            .recipe-card{
+                margin:10px;
+
+            }
+        }
+        @media (max-width:450px){
+            .display-recipe{
+                padding:5px;
+            }
+        } */
+        .modal-pop{
+            position:fixed;
+            width:100%;
+            height:100%;
+            top:0;
+            left:0;
+            right:0;
+            bottom:0;
+            background-color:rgba(0,0,0,0.4);
+            
+        }
+        .recipe-modal-scroll{
+            width:70px;
+        }
+        .recipe-modal-scroll-track{
+            background-color:black;
+        }
+        .recipe-modal-scroll-thumb{
+            background-color:#ccc;
+        }
+        .close{
+            float:right;
+            font-size:35px;
+            font-weight:bold;
+            cursor:pointer;
+            transition-duration:0.6s;
+        }
+        .close:hover{
+            color:white;
+        }
+        .hide{
+            display:none;
+        }
+        .show{
+            display:block;
+        }
+        .recipe-modal{
+            position:fixed;
+            top:0%;
+            left:20%;
+            bottom:0%;
+            background-color:orange;
+            height:90%;
+            border-radius:8px;
+            padding:40px;
+            overflow-y:scroll;
+            width:750px;
+        }
+        .recipe-modal p{
+            font-size:18px;
+            word-wrap:break-word;
+        }
+        .modal-img{
+            width:400px;
+            height:250px;
+            box-sizing:border-box;
+        }
+        .pop-img{
+            width:400px;
+            height:250px;
+        }
     </style>
 </head>
 <body>
+<div class="modal-pop" style="display:none;">
+    
+    <div class="recipe-modal">
+        <span class="close" onclick="parentNode.parentNode.style.display='none';">&times;</span><br>
+        <div class="modal-img">
+            <img class="pop-img" src="https://www.themealdb.com/images/media/meals/1529444830.jpg" />
+        </div><br>
+        <h1>Recipe Instructions</h1><br>
+        <p>Preheat oven to 350° F. Spray a 9x13-inch baking pan with non-stick spray.\r\nCombine soy sauce, ½ cup water, brown sugar, ginger and garlic 
+            in a small saucepan and cover. Bring to a boil over medium heat. Remove lid and cook for one minute once boiling.\r\nMeanwhile, stir together the corn starch and 
+            2 tablespoons of water in a separate dish until smooth. 
+            Once sauce is boiling, add mixture to the saucepan and stir to combine. Cook until the sauce starts to thicken then remove from heat.\r\nPlace the
+             chicken breasts in the prepared pan. Pour one cup of the sauce over top of chicken. Place chicken in oven and bake 35 minutes or until cooked through. Remove
+              from oven and shred chicken in the dish
+             using two forks.\r\n*Meanwhile, steam or cook the vegetables according to package directions.\r\nAdd the cooked vegetables and rice to the casserole dish with 
+             the chicken. Add most of the remaining sauce, reserving a bit to drizzle over the top when serving. Gently toss everything together in the casserole dish until 
+             combined. Return to oven and cook 15 minutes. Remove from oven and let stand 5 minutes before serving. Drizzle each serving with remaining sauce. Enjoy!</p><br>
+        <center><a href="#" >Watch Video</a></center><br><br><br>
+    </div>
+    </div>
     <?php 
         $ingredient="egg";
         if(isset($_POST['submit'])){
@@ -99,7 +195,7 @@
                 $dish_img=$json['meals'][$i]['strMealThumb'];
                 $mealid=$json['meals'][$i]['idMeal'];
                 echo 
-                "<div class=\"recipe-card\">
+                "<div class=\"recipe-card\" id=\"$mealid\">
                     <div class=\"recipe-img\">
                         <img class=\"source-img\" src=\"$dish_img\" /><br>
                     </div><br><br>
@@ -119,5 +215,46 @@
     
         
     </div>
+    
+    <script>        
+        let mealList=document.querySelector(".display-recipe")
+            // console.log(mealList)
+            mealList.addEventListener('click', function(e){
+                e.preventDefault();
+                if(e.target.classList.contains('try-new')){
+                    let popupcontent = document.querySelector(".modal-pop")
+                    // console.log(e.target.parentNode.parentNode.parentNode)
+                    const id=e.target.parentNode.parentNode.parentNode.id
+                    // console.log(id)
+                    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+                    .then(response => response.json())
+                    .then(data=>{
+                        // console.log(data)
+                        // console.log(data['meals'][0])
+                        mealInstructions=data['meals'][0]['strInstructions']
+                        mealImg=data['meals'][0]['strMealThumb']
+                        mealName=data['meals'][0]['strMeal']
+                        youtubeLink=data['meals'][0]['strYoutube']
+                        // console.log(mealInstructions)
+                        html=`<div class="recipe-modal">
+                            <label class="close" onclick="parentNode.parentNode.style.display='none';">&times;</label><br>
+                                <div class="modal-img">
+                                    <img class="pop-img" src="${mealImg}" />
+                                </div><br>
+                                <h1>${mealName} Recipe Instructions</h1><br>
+                                <p>${mealInstructions}</p><br>
+                                <center><a target="_blank" href="${youtubeLink}" >Watch Video</a></center><br><br><br>
+                            </div>`;
+                            popupcontent.innerHTML='';
+                        popupcontent.style.display="block";
+                        popupcontent.innerHTML=html;
+                    })
+                }
+            })
+            
+            
+            
+    </script>
+
 </body>
 </html>
